93f80569ce7adeaeb212610c6435dae69242e669
Wadim Sagaidak <grusa1360@gmail.com>
2025-02-26 14:13:06 +0200

    add to app.c

--- app.c
+++ app.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #define F 2025
+#define S 1991
 int main()
 {
     printf("Hello Git in %d\n!", F);
+    printf("Haifa : %d\n", S);
     put("______________\n");
     return 0;
 }
