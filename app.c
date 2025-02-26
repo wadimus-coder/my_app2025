79e5254dce2f24c51a4595dc746c06308db78d1b
Wadim Sagaidak <grusa1360@gmail.com>
2025-02-26 20:50:54 +0200

    Addition in the file app.c

--- app.c
+++ app.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #define F 2025
 #define S 1991
+
+void hello();
+
 int main()
 {
+    hello();
     printf("Hello Git in %d\n!", F);
     printf("Haifa : %d\n", S);
     put("______________\n");
     return 0;
+}
+
+void hello()
+{
+
+    printf("My name is Wadim!\n");
+    return;
 }
